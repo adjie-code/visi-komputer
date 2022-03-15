e95cb836a734493e9b06d6eefa333d9ac7c457cc
adjie-code <59765402+adjie-code@users.noreply.github.com>
2022-03-15 22:51:20 +0700

    Add files via upload

---
+++ TBOP523420025/src/tbop523420025/TBOP523420025.java
@@ -0,0 +1,23 @@
+
+
+package tbop523420025;
+import java.util.Scanner;
+
+public class TBOP523420025 {
+     static Scanner input = new Scanner (System.in);
+   
+    public static void main(String[] args) {
+        String b;
+        
+         System.out.println(" Stemmer Kata Sederhana ");
+         System.out.println("---------------------------");
+         System.out.println("---Klik Enter Untuk Stemming---");
+         System.out.print("Masukkan Kata :");
+         b = input.nextLine();
+         System.out.println("---------------------------");
+         System.out.println("Hasil Stemming: "+b);
+         
+         
+    }
+    
+}
